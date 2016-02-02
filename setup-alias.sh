60044c6206dbc1b09c183920ff48d12ca66c164e
Caio Cruz <cainho8@gmail.com>
2016-02-02 10:00:15 -0200

    - Add log color alias. - Add ignore file alias.

--- setup-alias.sh
+++ setup-alias.sh
@@ -2,3 +2,7 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
+git config --global alias.lg 'log --graph --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold white)- %an%C(reset)%C(bold yellow)%d%C(reset)" --abbrev-commit --date=relative'
+git config --global alias.l 'log --pretty=format:"(%C(yellow)%h%Creset) %C(cyan)%cn%Creset on %C(magenta)%cd%Creset %Cgreen%s"'
+git config --global alias.lfs 'log --pretty=format:"(%C(yellow)%h%Creset) %C(cyan)%cn%Creset on %C(magenta)%cd%Creset %Cgreen%s" --name-status'
+git config --global alias.ignorefile 'update-index --assume-unchanged'
